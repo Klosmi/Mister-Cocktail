f324d734635c0330020d954971be11e9c2bf7d16
Miklos Ferencz <miklos.ferencz@gmail.com>
2021-03-04 16:33:44 +0100

    index

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,4 @@
 class Ingredient < ApplicationRecord
-  belongs_to :cocktail
+  has_many :doses
+  validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_115652) do
+ActiveRecord::Schema.define(version: 2021_03_04_131531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,13 +21,22 @@ ActiveRecord::Schema.define(version: 2021_03_04_115652) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "ingredients", force: :cascade do |t|
-    t.string "name"
+  create_table "doses", force: :cascade do |t|
+    t.string "description"
+    t.bigint "ingredient_id", null: false
     t.bigint "cocktail_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["cocktail_id"], name: "index_ingredients_on_cocktail_id"
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
-  add_foreign_key "ingredients", "cocktails"
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
