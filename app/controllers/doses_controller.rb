1062e1883ac930bbf71e6f687a02dc2a672228d3
Miklos Ferencz <miklos.ferencz@gmail.com>
2021-03-05 18:02:53 +0100

    pulling up to heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,31 @@
 class DosesController < ApplicationController
-  before_action :find_cocktail
-
+  # cocktails/:cocktail_id/doses/new
   def new
-    @dose = Dose.new
-    @cocktail = Cocktail.find(params[:id])
+      @dose = Dose.new
+      @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.delete
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
 end
